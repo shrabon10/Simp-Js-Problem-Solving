1aefcb8ba00a1e0b4fa8e761e0ea046af8826ae4
shrabon10 <omarfaruksrabon@gmail.com>
2026-02-02 23:35:20 +0600

    fixed some code

--- js-problem-solving-02/shoppingCard.js
+++ js-problem-solving-02/shoppingCard.js
@@ -1,21 +1,21 @@
 const product = [
     {name : 'Shampoo', price : 200, quantity : 5},
-    {name : 'shrit', price : 900, quantity : 2},
+    {name : 'shrit', price : 500, quantity : 3},
     {name : 'sunglass', price : 700, quantity : 4},
     {name : 'facewash', price : 500, quantity : 2},
 ]
 
-function Prices (product){
-    let item = product[0]
+function cartTotal(product){
+    let total = 0
     for(const products of product){
-        if (item.Prices < product.price ) {
-            item = products
-        }
-    } 
-    return item
+        const thisproductCost = products.price * products.quantity;
+        total = total + thisproductCost
+    }
+    return total
 }
 
-const chep = Prices(product)
-console.log(chep);
+const bill = cartTotal(product)
+console.log("Your bill is: ", bill);
+
 
 

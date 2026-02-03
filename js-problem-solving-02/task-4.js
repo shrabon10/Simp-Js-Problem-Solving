dd3dfd50bebf477c8e8d32cd9d354fce67caed5e
shrabon10 <omarfaruksrabon@gmail.com>
2026-02-03 12:17:17 +0600

    task-4 has been completed

---
+++ js-problem-solving-02/task-4.js
@@ -0,0 +1,21 @@
+function findAveragePhonePrice(phones) {
+  let totalPrice = 0;
+
+  for (const phone of phones) {
+    totalPrice += phone.price;
+  }
+
+  return totalPrice / phones.length;
+}
+
+
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+console.log(findAveragePhonePrice(phones));
